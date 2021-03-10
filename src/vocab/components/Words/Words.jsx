330de6cc98c964daf41f1f16cb9ffe9219b4afe1
Louis Li <louisrli@gmail.com>
2021-03-10 18:50:39 +0000

    reset max

--- src/vocab/components/Words/Words.jsx
+++ src/vocab/components/Words/Words.jsx
@@ -18,7 +18,7 @@ import styles from './Words.css';
  * lookups. After this many failures, we assume there is something wrong with
  * the user's dictionary or language config.
  */
-const MAX_CONSECUTIVE_LOOKUP_FAILURES = 1;
+const MAX_CONSECUTIVE_LOOKUP_FAILURES = 10;
 
 /**
  * Words component
@@ -56,6 +56,19 @@ export default class Words extends PureComponent {
   exportDeck(exportType) {
     this.setState({ exportType });
   }
+
+  changeHeadWord(item, value) {
+    VocabStore.updateItem(this.props.id, item, { selection: value,
+      def: item.def.map(defSubitem => {
+        return {
+          ...defSubitem,
+          // Rewrite def with new selection value.
+          text: value,
+        };
+      })
+    });
+  }
+
 
   changeDef(item, value) {
     VocabStore.updateItem(this.props.id, item, { def: [
@@ -103,6 +116,7 @@ export default class Words extends PureComponent {
         return;
       }
       if (lookupFailureStreak > MAX_CONSECUTIVE_LOOKUP_FAILURES) {
+        setIsFinishedFetching();
         return;
       }
 
@@ -187,7 +201,7 @@ export default class Words extends PureComponent {
             <HeadWord
               lang={ deck.lang }
               def={ item.def }
-              onChange={ val => this.changeHeadWord }
+              onChange={ val => this.changeHeadWord(item, val) }
             />
           </div>
 
